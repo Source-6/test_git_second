94617d400dc18e73660bdf35dda56c4aa0943e31
Msource <m.vandermeirsch@interface3.be>
2025-06-06 10:07:13 +0200

    second repos

---
+++ helloworld.py
@@ -0,0 +1,18 @@
+from random import randint
+
+print("hello, wolrd !!")
+
+print(randint (1,6))
+
+print("this is the end")
+print(" *>* ")
+
+print(r"""                           
+ (                         
+ )\    )        (          
+((_)( /(  `  )  )\   (     
+ _  )(_)) /(/( ((_)  )\ )  
+| |((_)_ ((_)_\ (_) _(_/(  
+| |/ _` || '_ \)| || ' \)) 
+|_|\__,_|| .__/ |_||_||_|  
+         |_|               """)
